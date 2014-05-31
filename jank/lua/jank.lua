dfa39bb332fc8cc773f4f2eee8d93ab2c1e5b34f
ruffenman <jorge@jorgeccapote.com>
2014-05-31 17:33:59 -0400

    Scales and voicings on triggers now match settings more clearly
    Signed-off-by: ruffenman <jorge@jorgeccapote.com>

--- jank/lua/jank.lua
+++ jank/lua/jank.lua
@@ -2,12 +2,17 @@ local maximua
 local luaXinput
 local osc1, osc2, osc3, osc4
 local state, leftTrigger, rightTrigger, leftX, leftY, rightX, rightY
+-- Chromatic notes
 local notes = { 16.35, 17.32, 18.35, 19.45, 20.60, 21.83, 23.12, 24.50, 25.96, 27.5, 29.14, 30.87 }
 
+-- Index of chromatic note that defines the root of the key
 local key = 10
+-- Which octave is the root note in
 local octave = 3
-local scale = { 1, 3, 5, 6, 8, 10, 12 }
-local voicing = { 1, 5, 10, 14 }
+-- Scale by indexing chromatic notes
+local scale = { 1, 3, 5, 6, 8, 10, 12, 13, 15, 17, 18, 20, 22, 24, 25 }
+-- Voicing by indexing scale notes
+local voicing = { 1, 5, 7, 10, 12, 15 }
 
 local output = 0
 
@@ -74,7 +79,7 @@ function getSample(isLeft)
     -- Convert input to 0 - 1
     local input = leftTrigger / 255
     -- Calc step of note    
-    local step = lerp(1, 2 * #voicing + 1, input)
+    local step = lerp(1, #voicing, input)
     step = math.floor(step)
     freq = getFreq(key, octave, innerToOuterStep(scale, notes, innerToOuterStep(voicing, scale, step)))
     return osc1.sinewave(freq);
@@ -82,7 +87,7 @@ function getSample(isLeft)
     -- Convert input to 0 - 1
     local input = rightTrigger / 255
     -- Calc step of note
-    local step = lerp(1, 2 * #scale + 1, input)
+    local step = lerp(1, #scale, input)
     step = math.floor(step)
     freq = getFreq(key, octave, innerToOuterStep(scale, notes, step))
     return osc2.sinewave(freq);
